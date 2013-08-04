6424e5d5e018d42ef85560779591234a0b41af47
Iacopo Spalletti <i.spalletti@nephila.it>
2013-08-04 15:08:14 +0200

    First sample test

--- djangocms_export_objects/tests/base.py
+++ djangocms_export_objects/tests/base.py
@@ -12,4 +12,27 @@ from six import StringIO
 
 
 class TestsBase(unittest.TestCase):
-    pass
+
+    def create_fixtures(self):
+        """
+        Tree from fixture:
+
+            page1
+                page2
+                    page3
+            page4
+                page5
+        """
+        from cms.api import create_page
+        from cms.models import Page
+        defaults = {
+            'template': 'index.html',
+            'language': 'en',
+        }
+        p1 = create_page('page1', published=True, in_navigation=True, **defaults)
+        p4 = create_page('page4', published=True, in_navigation=True, **defaults)
+        p1 = Page.objects.get(pk=p1.pk)
+        p2 = create_page('page2', published=True, in_navigation=True, parent=p1, **defaults)
+        create_page('page3', published=True, in_navigation=True, parent=p2, **defaults)
+        p4 = Page.objects.get(pk=p4.pk)
+        create_page('page5', published=True, in_navigation=True, parent=p4, **defaults)
--- djangocms_export_objects/tests/dump.py
+++ djangocms_export_objects/tests/dump.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
+from io import BytesIO
+
+from django.core.management import call_command
+
 from .base import TestsBase
+
 
 class TestDjangocmsExportObjects(TestsBase):
 
     def setUp(self):
-        pass
+        self.create_fixtures()
 
-    def test_x(self):
-        pass
+    def test_simple_dump(self):
+        jdata = BytesIO()
+        call_command("cms_dump_objects", "cms.Page.objects.all()", stdout=jdata, indent=3)
+        data = json.loads(jdata.getvalue())
+
+        page_items = [item for item in data if item['model'] == 'cms.page']
+        # 5 pages, 10 page objects
+        self.assertEqual(len(page_items), 10)
+
+        title_items = [item for item in data if item['model'] == 'cms.title']
+        # 5 pages, 10 titles
+        self.assertEqual(len(title_items), 10)
+
 
     def tearDown(self):
         pass
