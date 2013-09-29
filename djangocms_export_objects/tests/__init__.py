78824c0bf1117d5a9398f4dfe5fa8c8f81ca31d1
Iacopo Spalletti <i.spalletti@nephila.it>
2013-09-29 12:46:16 +0200

    Add doc testing

--- djangocms_export_objects/tests/__init__.py
+++ djangocms_export_objects/tests/__init__.py
@@ -1,1 +1,2 @@
 from .dump import *
+from .docs import *
---
+++ djangocms_export_objects/tests/docs.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+import os
+import socket
+
+from sphinx.application import Sphinx
+from six import StringIO
+
+from .base import unittest
+from .tmpdir import temp_dir
+from unittest import skipIf
+
+ROOT_DIR = os.path.dirname(__file__)
+DOCS_DIR = os.path.abspath(os.path.join(ROOT_DIR, u'..', u'..', u'docs'))
+
+
+def has_no_internet():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('4.4.4.2', 80))
+        s.send(b"hello")
+    except socket.error: # no internet
+        return True
+    return False
+
+
+class DocsTestCase(unittest.TestCase):
+    """
+    Test docs building correctly for HTML
+    """
+    @skipIf(has_no_internet(), "No internet")
+    def test_html(self):
+        nullout = StringIO()
+        with temp_dir() as OUT_DIR:
+            app = Sphinx(
+                DOCS_DIR,
+                DOCS_DIR,
+                OUT_DIR,
+                OUT_DIR,
+                "html",
+                warningiserror=False,
+                status=nullout,
+            )
+            try:
+                app.build()
+            except:
+                print(nullout.getvalue())
+                raise
